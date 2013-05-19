e82e21bafd20f7fad681314d8914c74e6acfe725
Anton Beloglazov <anton.beloglazov@gmail.com>
2013-05-19 12:19:45 +1000

    Added handling of not found cases

--- plugin/thesaurus-lookup.sh
+++ plugin/thesaurus-lookup.sh
@@ -7,25 +7,29 @@
 
 wget -q http://thesaurus.com/browse/${1} -O src.tmp
 
-echo -n "Main entry: "
-cat src.tmp | \
-    grep 'queryn' | \
-    sed -n -e 's/.*b>\(.*\)<\/b>.*/\1/p'
+if cat src.tmp | grep 'queryn' > /dev/null; then
+    echo -n 'Main entry: '
+    cat src.tmp | \
+        grep 'queryn' | \
+        sed -n -e 's/.*b>\(.*\)<\/b>.*/\1/p'
 
-echo -n "Definition: "
-cat src.tmp | \
-    grep -m2 -A1 'Definition' | \
-    sed -n -e 's/<td>\(.*\)<\/td.*/\1/p'
+    echo -n 'Definition: '
+    cat src.tmp | \
+        grep -m2 -A1 'Definition' | \
+        sed -n -e 's/<td>\(.*\)<\/td.*/\1/p'
 
-echo -n "Synonyms: "
-cat src.tmp | \
-    sed -n '/<td valign="top">Synonyms:<\/td>/,/\/span/ {/\/span/q; p;}' | \
-    sed 's/.*<td.*//g' | \
-    sed 's/<[^>]*>//g' | \
-    tr -d '*' | \
-    tr '\n' ' ' | \
-    sed -e 's/^ *//' | \
-    sed -e 's/ *$//'
+    echo -n 'Synonyms: '
+    cat src.tmp | \
+        sed -n '/<td valign="top">Synonyms:<\/td>/,/\/span/ {/\/span/q; p;}' | \
+        sed 's/.*<td.*//g' | \
+        sed 's/<[^>]*>//g' | \
+        tr -d '*' | \
+        tr '\n' ' ' | \
+        sed -e 's/^ *//' | \
+        sed -e 's/ *$//'
+else
+    echo "The word \"${1}\" has not been found on thesaurus.com!"
+fi
 
 rm src.tmp
 
