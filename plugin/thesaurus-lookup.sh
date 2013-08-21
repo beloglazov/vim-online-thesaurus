3bb3fe9dead992f2df215a0ea0538ff90c39671a
Anton Beloglazov <anton.beloglazov@gmail.com>
2013-08-21 23:44:12 +1000

    Fixed thesaurus-lookup.sh to work correctly with the new version of thesaurus.com; bumped up the version to 0.1.4

--- plugin/thesaurus-lookup.sh
+++ plugin/thesaurus-lookup.sh
@@ -2,36 +2,30 @@
 
 # Vim plugin for looking up words in an online thesaurus
 # Author:       Anton Beloglazov <http://beloglazov.info/>
-# Version:      0.1.3
+# Version:      0.1.4
 # Original idea and code: Nick Coleman <http://www.nickcoleman.org/>
 
 curl -o src.tmp http://thesaurus.com/browse/${1} >/dev/null 2>&1
 
-if cat src.tmp | grep 'queryn' > /dev/null; then
+if ! grep 'no thesaurus results' src.tmp > /dev/null; then
     echo -n 'Main entry: '
-    cat src.tmp | \
-        grep 'queryn' | \
-        sed -n -e 's/.*b>\(.*\)<\/b>.*/\1/p'
+    echo ${1} | tr '[A-Z]' '[a-z]'
 
     echo -n 'Definition: '
-    cat src.tmp | \
-        grep -m2 -A1 'Definition' | \
-        sed -n -e 's/<td>\(.*\)<\/td.*/\1/p' | \
+    awk -F'<|>' '/layer disabled/ {flag=0}; \
+        flag && /ttl/ {print $3; flag=0}; \
+        /words-gallery/ {flag=1}' src.tmp
+
+    echo -n 'Synonyms: '
+    awk -F'<|>|&quot;' 'flag && /<\/div>/ {flag=0; done=1}; \
+        flag && !done && /text/ {printf "%s ",$5; print $13};
+        /relevancy-list/ {flag=1}' src.tmp | \
+        sort -t ' ' -k 1,1r -k 2,2 | \
+        sed 's/relevant-[0-9]* //g' | \
         sed 's/$/, /g' | \
         tr -d '\n' | \
         sed 's/, *$//' | \
         tr -d '\n'
-
-    echo
-    echo -n 'Synonyms: '
-    cat src.tmp | \
-        sed -n '/<td valign="top">Synonyms:<\/td>/,/\/span/ {/\/span/q; p;}' | \
-        sed 's/.*<td.*//g' | \
-        sed 's/<[^>]*>//g' | \
-        tr -d '*' | \
-        tr '\n' ' ' | \
-        sed -e 's/^ *//' | \
-        sed -e 's/ *$//'
 else
     echo "The word \"${1}\" has not been found on thesaurus.com!"
 fi
